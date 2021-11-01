e0e36e211e9eb3099d6370a68ad780b0ce5b9fb2
KareninaK <kareninakauff@hotmail.com.ar>
2021-11-01 11:30:32 -0300

    process

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen("https://pi-dogs-nina.herokuapp.com", () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
