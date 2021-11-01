d8fdc9d63f3370f31231e470d03a51e3ad1b9fbd
KareninaK <kareninakauff@hotmail.com.ar>
2021-11-01 10:52:46 -0300

    ver

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
-
+//chau
 ReactDOM.render(
   <Provider store={store}> 
     <React.StrictMode>
